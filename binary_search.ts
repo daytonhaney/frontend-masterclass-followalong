7fcb6f89ae578c0f05d3adb1ca6181c56e4ffd3a
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-10-05 22:20:40 -0400

    Update binary_search.ts

--- binary_search.ts
+++ binary_search.ts
@@ -4,19 +4,18 @@ export default function bs_list(haystack: number[], needle: number): boolean {
     let high = haystack.length;
 
     do{
-        const m = Math.floor(low + (high - low )/ 2);
+        const m = Math.floor(low + (high - low )/ 2); // m = midpoint of needle 
         const v = haystack[m]; 
         
-        if (v == needle){
+        if (v == needle){ // if value == needle , midpoint is found
             return true; 
-        } else if (v > needle ){
+        } else if (v > needle ){  // value is greater than needle... 
             high = m;
         } else {
-          low = m + 1;
+          low = m + 1; // dropping the midpoint (exclude right "high " side )
     }  
-    
+        
     }while (low < high);
        return false
-  
 }
 
